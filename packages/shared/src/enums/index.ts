ea90cc484908cd9a88c71c7890cf043ed04bc2a3
luantran <the-luan.tran@mail.mcgill.ca>
2026-05-28 12:54:02 -0400

    set up thje enums

---
+++ packages/shared/src/enums/index.ts
@@ -0,0 +1,21 @@
+export enum UserRole {
+  Teacher = "teacher",
+  Student = "student",
+}
+
+export enum ExerciseType {
+  Standalone = "standalone",
+  Context = "context",
+}
+
+export enum ExerciseCategory {
+  Grammar = "grammar",
+  Conjugaison = "conjugaison",
+  Reading = "reading",
+}
+
+export enum QuestionType {
+  MCQ = "mcq",
+  Fill = "fill_in_gap",
+  Short = "short_answer",
+}
