ec4eafb2a3725a0cb5e134465df96a0adcfdf0f5
Luan Tran <the-luan.tran@mail.mcgill.ca>
2026-06-26 11:11:26 -0400

    fixed small issues

--- packages/web/playwright.config.ts
+++ packages/web/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   webServer: {
-    command: "npm run dev",
+    command: "npm run dev -- --port 5173 --strictPort",
     url: "http://localhost:5173",
     reuseExistingServer: !process.env.CI,
   },
